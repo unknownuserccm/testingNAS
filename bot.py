cb5daf224ad6b2daae1b17d034ca445f55484904
ccm <ccmandccs@gmail.com>
2025-04-29 17:48:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,4 +16,4 @@ async def hello(ctx):
     await ctx.send(f'Hello {ctx.author.display_name}!')
 
 # Run the bot with your bot token
-bot.run('MTM2NjcxMDk5ODk0OTY5MTQ1Mg.GJHd2n.0cUNcSO5DBlv3oppUWiqYw-hC6rU0iPml33A2o')  # Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot.run('DISCORD_TOKEN')  # Replace 'YOUR_BOT_TOKEN' with your actual bot token
